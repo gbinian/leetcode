5e900d4820ff4f15dfb4bfbc713e38e7c49659f9
gbinian <gbinian@outlook.com>
2021-03-01 22:55:55 +0800

    双指针

--- src/simple.rs
+++ src/simple.rs
@@ -3,34 +3,31 @@
 /// 不要使用额外的数组空间，你必须在原地修改输入数组 并在使用 O(1) 额外空间的条件下完成。
 ///
 pub fn remove_duplicates(nums: &mut Vec<i32>) -> i32 {
-		if nums.is_empty() {
-			return 0
-		}
-		let mut p1 = 0;
-		let mut p2 = 1;
-		let mut i = 0;
-		while p2 < nums.len() {
-			if nums[p2] == nums[p1] {
-				p2 +=1;
-			} else {
-				p1 += 1;
-				nums[p1] = nums[p2];
-				i += 1;
-			}
-		}
-		i + 1
+    if nums.is_empty() {
+        return 0;
+    }
+    let mut p1 = 0;
+    let mut p2 = 1;
+    let mut i = 0;
+    while p2 < nums.len() {
+        if nums[p2] == nums[p1] {
+            p2 += 1;
+        } else {
+            p1 += 1;
+            nums[p1] = nums[p2];
+            i += 1;
+        }
+    }
+    i + 1
 }
-
 
 #[cfg(test)]
 mod tests {
-	use super::*;
-	#[test]
-	fn test_remove_duplicates() {
-		let mut v = vec![1,1,2,3,4,4,5];
-		let res = remove_duplicates(&mut v);
-		assert_eq!(v, vec![1,2,3,4,5]);
-		assert_eq!(res, 5);
-	}
-	
+    use super::*;
+    #[test]
+    fn test_remove_duplicates() {
+        let mut v = vec![1, 1, 2, 3, 4, 4, 5];
+        let res = remove_duplicates(&mut v);
+        assert_eq!(res, 5);
+    }
 }
